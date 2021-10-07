6035efef5f7122b99d91a0cfabe8b3a49eadd7ff
Noah Gift <noah.gift@gmail.com>
2021-10-07 15:32:46 +0000

    adding change

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+
 def hello(name):
     return f"Your name is: {name}"
 
-#This catches a future bug
-1==1
+
+# This catches a future bug
+#1 == 1
 
 if __name__ == "__main__":
     print(hello("bob"))
